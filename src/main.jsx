d7c11d052288d6fe13d09b94e04562c8c9d32a7f
abhi-zero <abhi.0zero@gmail.com>
2025-08-17 18:38:27 +0530

    main file

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import { Provider } from "react-redux";
+import store from "./app/store.js";
+import Country from "./pages/Country.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/country/:countryName",
+        element: <Country />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
