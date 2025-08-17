833e4f38bfd4e9b73552c0b9cbfbb070e846da75
abhi-zero <abhi.0zero@gmail.com>
2025-08-17 18:36:05 +0530

    Index file to import component from one location

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import Header from './Header/Header'
+import InputText from './InputText'
+import Dropdown from './Dropdown'
+import Card from './Card'
+import Backbtn from './Backbtn'
+import Flag from './Flag'
+import CountryDetails from './CountryDetails'
+
+
+export {
+    Header,
+    InputText,
+    Dropdown,
+    Card,
+    Backbtn,
+    Flag,
+    CountryDetails
+}
